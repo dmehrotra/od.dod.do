1ebee890a4f6c8153b4d1a8ae210655ab3e1b1f4
dmehrotra <dhruv.mehrotra3@gmail.com>
2018-05-15 18:38:46 +0000

    working lite-import

--- v2/import-filing/server/migrations/20180514202356-create-filing.js
+++ v2/import-filing/server/migrations/20180514202356-create-filing.js
@@ -13,7 +13,7 @@ module.exports = {
         type: Sequelize.FLOAT
       },
       full_text: {
-        type: Sequelize.STRING
+        type: Sequelize.TEXT
       },
       createdAt: {
         allowNull: false,
